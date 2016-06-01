1c0bfa935ee8052bde1e3d1eb0da764fb5ffd677
Jeremy <saturn5971@gmail.com>
2016-06-01 00:33:10 -0400

    Added basic memory game functionality
    Can click on pairs of cards to flip them over.
    
    When the pair matches (i.e., when they're the same color) the pair
    disappears after a set time. When the pair doesn't match, they stay
    turned over for a moment, but then everything returns to the way it was.

--- game.js
+++ game.js
@@ -1,38 +1,80 @@
 var Card = React.createClass({
+    handleClick: function() {
+                     this.props.handleClick(this.props.myId);
+                 },
+
     render: function() {
-        var style = {
-            backgroundColor: 'lightcyan',
-            display: 'inline-block',
-            margin: this.props.margin,
-            height: this.props.height,
-            width: this.props.width
-        };
+                var backgroundColor = this.props.isSelected ? this.props.image : 'lightCyan';
+                var visibility = this.props.wasMatched ? 'hidden' : 'visible';
 
-        return (
-            <div className="card" style={style}></div>
-        );
-    },
+                var style = {
+                    backgroundColor: backgroundColor,
+                    display: 'inline-block',
+                    margin: this.props.margin,
+                    visibility: visibility,
+                    height: this.props.height,
+                    width: this.props.width
+                };
 
+                return (
+                    <div className="card" style={style} onClick={this.handleClick}></div>
+                );
+            },
 });
 
 var Game = React.createClass({
-    render: function() {
-                var cards = [];
+    evaluateMatch: function() {
+                    var matched = this.state.matched.slice();
 
+                    var a = this.state.selected[0];
+                    var b = this.state.selected[1];
+
+                    if (this.props.images[a] === this.props.images[b]) {
+                        matched = matched.concat([a, b]);
+                    }
+
+                    this.setState({ selected: [], matched: matched });
+                    this.resetTimer = null;
+                },
+
+    getInitialState: function() {
+                         return {selected: [], matched: []}
+                     },
+
+    handleCardClick: function(cardId) {
+                         if (this.resetTimer || this.state.selected.includes(cardId)) {
+                             return;
+                         }
+
+                         if (this.state.selected.length >= 1) {
+                             this.resetTimer = setTimeout(this.evaluateMatch, 1000);
+                         }
+
+                         this.setState({ selected: this.state.selected.concat(cardId) });
+                 },
+
+    render: function() {
                 var style = {
                     backgroundColor: 'salmon',
                     width: this.props.cardCountX * (this.props.cardWidth + this.props.cardMargin * 2)
                 };
 
-                for (var i = 0; i < this.props.cardCountX * this.props.cardCountY; i++) {
+                var cards = [];
+                
+                this.props.images.forEach((image, i) => {
                     cards.push(
-                        <Card 
-                            key={i} 
+                        <Card
+                            handleClick={this.handleCardClick}
+                            image={image}
+                            key={i}
                             margin={this.props.cardMargin}
+                            myId={i}
+                            isSelected={this.state.selected.includes(i)}
+                            wasMatched={this.state.matched.includes(i)}
                             width={this.props.cardWidth} 
                             height={this.props.cardHeight}
                         />);
-                }
+                });
 
                 return (
                     <div className="game" style={style}>
@@ -42,13 +84,19 @@ var Game = React.createClass({
             }
 });
 
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo'];
+colors = colors.concat(colors);
+
 ReactDOM.render(
     <Game 
-        cardCountX={5}
+        cardCountX={3}
         cardCountY={4}
         cardMargin={5}
         cardHeight={100} 
         cardWidth={100} 
+        images={colors}
     />, 
-    document.getElementById('content'));
+    document.getElementById('content')
+);
 
