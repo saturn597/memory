6f5336cdbd6db72418181cc5e07f86613627e65d
Jeremy <saturn5971@gmail.com>
2016-06-18 14:51:44 -0400

    Switch to ES6 style classes

--- game.js
+++ game.js
@@ -1,104 +1,133 @@
 // React classes
-var Card = React.createClass({
-    render: function() {
-                const image = this.props.isSelected ? `url(${this.props.image})` : 'none';
-                const visibility = this.props.wasMatched ? 'hidden' : 'visible';
 
-                var style = {
-                    backgroundImage: image,
-                    visibility: visibility
-                };
+class Card extends React.Component {
+    render() {
+        const image = this.props.isSelected ? `url(${this.props.image})` : 'none';
+        const visibility = this.props.wasMatched ? 'hidden' : 'visible';
 
-                const onClick = () => this.props.handleClick(this.props.myId);
+        var style = {
+            backgroundImage: image,
+            visibility: visibility
+        };
 
-                return (
-                    <div
-                        className="card"
-                        style={style}
-                        onClick={onClick}
-                    >
-                    </div>
-                );
-            },
-});
+        return (
+            <div
+                className="card"
+                style={style}
+                onClick={this.props.handleClick}
+            >
+            </div>
+        );
+    }
+}
 
-var Game = React.createClass({
-    evaluateMatch: function() {
-                    var matched = this.state.matched.slice();
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
 
-                    const a = this.state.selected[0];
-                    const b = this.state.selected[1];
+        this.evaluateMatch = this.evaluateMatch.bind(this);
+        this.restart = this.restart.bind(this);
 
-                    if (this.props.images[a] === this.props.images[b]) {
-                        matched = matched.concat([a, b]);
-                    }
+        this.imageMap = this.props.images.slice();
+        this.imageMap = this.imageMap.concat(this.imageMap);
+        shuffle(this.imageMap);
 
-                    this.setState({
-                        selected: [],
-                        matched: matched,
-                        guesses: this.state.guesses + 1,
-                    });
+        this.resetTimer = null;
 
-                    this.resetTimer = null;
-                },
+        this.state = this.getCleanState();
+    }
 
-    getInitialState: function() {
-                         return {selected: [], matched: [], guesses: 0};
-                     },
+    evaluateMatch() {
+        var matched = this.state.matched.slice();
 
-    handleCardClick: function(cardId) {
-                         if (this.resetTimer || this.state.selected.includes(cardId)) {
-                             return;
-                         }
+        const selectedImages = this.state.selected.map(id => this.imageMap[id]);
 
-                         if (this.state.selected.length >= 1) {
-                             this.resetTimer = setTimeout(this.evaluateMatch, 500);
-                         }
+        if (selectedImages[0] === selectedImages[1]) {
+            matched = matched.concat(this.state.selected);
+        }
 
-                         this.setState({selected: this.state.selected.concat(cardId)});
-                 },
+        this.setState({
+            selected: [],
+            matched: matched,
+            guesses: this.state.guesses + 1
+        });
 
-    render: function() {
-                var cards = [];
+        this.resetTimer = null;
+    }
 
-                this.props.images.forEach((image, i) => {
-                    cards.push(
-                        <Card
-                            handleClick={this.handleCardClick}
-                            image={image}
-                            isSelected={this.state.selected.includes(i)}
-                            key={i}
-                            myId={i}
-                            wasMatched={this.state.matched.includes(i)}
-                        />);
-                });
+    getCleanState() {
+         return {selected: [], matched: [], guesses: 0};
+    }
 
-                return (
-                    <div className="game">
-                        {cards}
-                        <div>Guesses: {this.state.guesses}</div>
-                        <div>Remaining: {cards.length - this.state.matched.length}</div>
-                        <button onClick={this.restart}>Start again</button>
-                    </div>
-                    );
-            },
+    handleCardClick(cardId) {
+        if (this.resetTimer || this.state.selected.includes(cardId)) {
+            return;
+        }
 
-    restart: function() {
-                 this.setState(this.getInitialState());
-             }
-});
+        if (this.state.selected.length >= 1) {
+            this.resetTimer = setTimeout(this.evaluateMatch, 500);
+        }
 
+        this.setState({selected: this.state.selected.concat(cardId)});
+    }
+
+    render() {
+        const cards = this.imageMap.map((image, i) =>
+            <Card
+                handleClick={this.handleCardClick.bind(this, i)}
+                image={image}
+                isSelected={this.state.selected.includes(i)}
+                key={i}
+                wasMatched={this.state.matched.includes(i)}
+            />
+        );
+
+        return (
+                <div className="game">
+                    {cards}
+                    <div>Guesses: {this.state.guesses}</div>
+                    <div>Remaining: {cards.length - this.state.matched.length}</div>
+                    <button onClick={this.restart}>Start again</button>
+                </div>
+               );
+    }
+
+    restart() {
+        this.setState(this.getCleanState());
+    }
+}
 
 // Main
-var images = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg', 'e.jpg', 'f.jpg'].map(i => 'images/' + i);
-images = images.concat(images);
+var images = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'].map(i => 'images/' + i + '.jpg');
 
-shuffle(images);
+var promises = [];
 
-ReactDOM.render(<Game images={images} />, document.getElementById('content'));
+for (var i of images) {
+    promises.push(preload(i));
+}
+
+Promise.all(promises).then(() =>
+    ReactDOM.render(<Game images={images} />, document.getElementById('content'))
+).catch((failedImages) => {
+    console.log('failed to load some images:');
+    console.log(failedImages);
+});
 
 
 // Utility
+function preload(url) {
+    return new Promise(function(resolve, reject) {
+        var img = new Image();
+        img.onload = function() {
+            resolve(url);
+        }
+        img.onerror = function() {
+            reject(url);
+        }
+        img.src = url;
+    });
+}
+
 function shuffle(arr) {
     // Implementing Fisher-Yates shuffle:
     // https://en.wikipedia.org/wiki/Fisher-Yates_shuffle
